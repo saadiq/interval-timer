000309bd50fb70acdf56ad7e758c15ec836b9e4c
Saadiq Rodgers-King <saadiq@users.noreply.github.com>
2024-08-08 22:52:49 -0400

    Update WorkoutTimer.tsx

--- src/app/WorkoutTimer.tsx
+++ src/app/WorkoutTimer.tsx
@@ -33,7 +33,9 @@ const WorkoutTimerContent: React.FC = () => {
   return (
     <div className="workout-timer">
       <h1 className="text-xl sm:text-2xl lg:text-4xl font-bold mb-4 text-center">
-        {workout.type.charAt(0).toUpperCase() + workout.type.slice(1)} Workout
+      {workout.type === "amrap" 
+        ? "AMRAP Workout"
+        : `${workout.type.charAt(0).toUpperCase() + workout.type.slice(1)} Workout`}
       </h1>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         <div className="lg:col-span-2">
