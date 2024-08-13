df879ba1ea2395223212719c87afcf4a44fe97a1
Saadiq Rodgers-King <saadiq@users.noreply.github.com>
2024-08-13 14:42:17 -0400

    Update useWorkoutFactory.ts

--- src/hooks/useWorkoutFactory.ts
+++ src/hooks/useWorkoutFactory.ts
@@ -4,12 +4,12 @@ import { WorkoutFactory } from '@/workouts/WorkoutFactory';
 import { Workout } from '@/workouts/Workout';
 import { WorkoutData } from '@/workouts/types';
 
-export function useWorkoutFactory(workoutData: WorkoutData) {
+export function useWorkoutFactory(workoutData: WorkoutData, date: string) {
   const [workout, setWorkout] = useState<Workout | null>(null);
 
   useEffect(() => {
-    setWorkout(WorkoutFactory.createWorkout(workoutData));
-  }, [workoutData]);
+    setWorkout(WorkoutFactory.createWorkout(workoutData, date));
+  }, [workoutData, date]);
 
   return workout;
 }
